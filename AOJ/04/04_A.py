963459ed9d04d50aa67bade94cf382352cfaee6e
Kazuma764 <kazuma.ishida1618@gmail.com>
2022-08-23 21:15:30 +0900

    Third_commit

--- AOJ/04/04_A.py
+++ AOJ/04/04_A.py
@@ -1,7 +1,6 @@
-X = input()
-Y = X.split()
-list_i = [int(i) for i in Y]
-list_f = [float(j) for j in Y]
+X = input().split()
+list_i = [int(i) for i in X]
+list_f = [float(j) for j in X]
 
 a1, b1 = list_i[0], list_i[1]
 a2, b2 = list_f[0], list_f[1]
