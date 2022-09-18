df527a1e2e5b90f6ad3e6b4c5c8353165f744d0e
Kazuma764 <kazuma.ishida1618@gmail.com>
2022-09-18 22:12:55 +0900

    forth commit

--- AOJ/11/11_C.py
+++ AOJ/11/11_C.py
@@ -1,6 +1,95 @@
 list_dice = []
 
 
+class Dice:
+    def __init__(self, num):
+        self.num_a = num[0]
+        self.num_b = num[1]
+        self.num_c = num[2]
+        self.num_d = num[3]
+        self.num_e = num[4]
+        self.num_f = num[5]
+
+    def roll_N(self):
+        self.num_a, self.num_b, self.num_e, self.num_f = (
+            self.num_b,
+            self.num_f,
+            self.num_a,
+            self.num_e,
+        )
+
+    def roll_S(self):
+        self.num_a, self.num_b, self.num_e, self.num_f = (
+            self.num_e,
+            self.num_a,
+            self.num_f,
+            self.num_b,
+        )
+
+    def roll_E(self):
+        self.num_a, self.num_c, self.num_d, self.num_f = (
+            self.num_d,
+            self.num_a,
+            self.num_f,
+            self.num_c,
+        )
+
+    def roll_W(self):
+        self.num_a, self.num_c, self.num_d, self.num_f = (
+            self.num_c,
+            self.num_f,
+            self.num_a,
+            self.num_d,
+        )
+
+    def rotate(self):
+        self.num_b, self.num_c, self.num_d, self.num_e = (
+            self.num_d,
+            self.num_b,
+            self.num_e,
+            self.num_c,
+        )
+
+    def mk_same(self):
+        for i in range(4):
+            for j in range(4):
+                self.rotate()
+                self.check()
+            self.roll_N()
+
+        for i in range(4):
+            for j in range(4):
+                self.rotate()
+                self.check()
+            self.roll_E()
+
+    def check(self):
+        x = [self.num_a, self.num_b, self.num_c, self.num_d, self.num_e, self.num_f]
+        if x in list_dice:
+            return
+        else:
+            list_dice.append(x)
+
+    def confirm_same(self, num):  # ここに入れている意味がほぼない
+        if num in list_dice:
+            print("Yes")
+        else:
+            print("No")
+
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+dice = Dice(a)
+
+dice.mk_same()
+dice.confirm_same(b)
+
+
+"""
+list_dice = []
+
+
 def roll_N(x):
     dice = [0] * 6
     dice[0] = x[1]
@@ -63,22 +152,27 @@ def check(x):
         list_dice.append(x)
 
 
+def mk_same(x):
+    for i in range(4):
+        for j in range(4):
+            x = rotate(x)
+            check(x)
+        x = roll_N(x)
+
+    for i in range(4):
+        for j in range(4):
+            x = rotate(x)
+            check(x)
+        x = roll_E(x)
+
+
 x = list(map(int, input().split()))
 y = list(map(int, input().split()))
 
-for i in range(4):
-    for j in range(4):
-        x = rotate(x)
-        check(x)
-    x = roll_N(x)
-
-for i in range(4):
-    for j in range(4):
-        x = rotate(x)
-        check(x)
-    x = roll_E(x)
+mk_same(x)
 
 if y in list_dice:
     print("Yes")
 else:
     print("No")
+"""
--- AOJ/11/11_D.py
+++ AOJ/11/11_D.py
@@ -1,6 +1,106 @@
 list_dice = []
 
 
+class Dice:
+    def __init__(self, num):
+        self.num_a = num[0]
+        self.num_b = num[1]
+        self.num_c = num[2]
+        self.num_d = num[3]
+        self.num_e = num[4]
+        self.num_f = num[5]
+
+    def roll_N(self):
+        self.num_a, self.num_b, self.num_e, self.num_f = (
+            self.num_b,
+            self.num_f,
+            self.num_a,
+            self.num_e,
+        )
+
+    def roll_S(self):
+        self.num_a, self.num_b, self.num_e, self.num_f = (
+            self.num_e,
+            self.num_a,
+            self.num_f,
+            self.num_b,
+        )
+
+    def roll_E(self):
+        self.num_a, self.num_c, self.num_d, self.num_f = (
+            self.num_d,
+            self.num_a,
+            self.num_f,
+            self.num_c,
+        )
+
+    def roll_W(self):
+        self.num_a, self.num_c, self.num_d, self.num_f = (
+            self.num_c,
+            self.num_f,
+            self.num_a,
+            self.num_d,
+        )
+
+    def rotate(self):
+        self.num_b, self.num_c, self.num_d, self.num_e = (
+            self.num_d,
+            self.num_b,
+            self.num_e,
+            self.num_c,
+        )
+
+    def mk_same(self):
+        for i in range(4):
+            for j in range(4):
+                self.rotate()
+                self.check()
+            self.roll_N()
+
+        for i in range(4):
+            for j in range(4):
+                self.rotate()
+                self.check()
+            self.roll_E()
+
+    def check(self):
+        x = [self.num_a, self.num_b, self.num_c, self.num_d, self.num_e, self.num_f]
+        if x in list_dice:
+            return
+        else:
+            list_dice.append(x)
+
+    def compare_dices(self, num, n):
+        for i in range(n - 1):
+            flag = False
+            if num[i + 1] not in list_dice:
+                flag = True
+            else:
+                break
+        return flag
+
+
+dices = []
+
+n = int(input())
+for i in range(n):
+    x = list(map(int, input().split()))
+    dices.append(x)
+
+dice = Dice(dices[0])
+
+dice.mk_same()
+
+if dice.compare_dices(dices, n):
+    print("Yes")
+else:
+    print("No")
+
+
+"""
+list_dice = []
+
+
 def roll_N(x):
     dice = [0] * 6
     dice[0] = x[1]
@@ -95,3 +195,4 @@ if flag:
     print("Yes")
 else:
     print("No")
+"""
